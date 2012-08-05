507dc08db725081fe5b9bcb1989c14b474dbaff9
Daroth <daroth@braindead.fr>
2012-08-05 15:05:31 +0200

    revert uselees root path configuration

--- bookmark/__init__.py
+++ bookmark/__init__.py
@@ -6,11 +6,9 @@ from flask_login import LoginManager
 from flask_debugtoolbar import DebugToolbarExtension
 from bookmark.config import Configuration
 
-
-app = Flask(__name__, static_folder=Configuration.BASE_PATH+'/'+Configuration.STATIC_FOLDER)
+app = Flask(__name__, static_folder=Configuration.STATIC_FOLDER)
 app.config.from_object('bookmark.config.Configuration')
 app.config.from_envvar('SALMON_LOCAL_SETTINGS', silent=True)
-
 
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -22,8 +20,6 @@ from bookmark.blueprint import api
 from bookmark.blueprint import web
 from bookmark.blueprint import login
 
-base = app.config['BASE_PATH']
-print base
-app.register_blueprint(api, url_prefix='%s/api' % (base,))
-app.register_blueprint(web, url_prefix='%s/' % (base,))
-app.register_blueprint(login, url_prefix='%s/user' % (base,))
+app.register_blueprint(api, url_prefix='/api')
+app.register_blueprint(web, url_prefix='/')
+app.register_blueprint(login, url_prefix='/user')
