5c0d88f3caac66405db843bc7dee2e0c695452a7
Daroth <daroth@braindead.fr>
2012-08-05 14:11:39 +0200

    fix static files access

--- bookmark/__init__.py
+++ bookmark/__init__.py
@@ -7,7 +7,7 @@ from flask_debugtoolbar import DebugToolbarExtension
 from bookmark.config import Configuration
 
 
-app = Flask(__name__, static_folder=Configuration.STATIC_FOLDER)
+app = Flask(__name__, static_folder=Configuration.BASE_PATH+'/'+Configuration.STATIC_FOLDER)
 app.config.from_object('bookmark.config.Configuration')
 app.config.from_envvar('SALMON_LOCAL_SETTINGS', silent=True)
 
@@ -23,6 +23,7 @@ from bookmark.blueprint import web
 from bookmark.blueprint import login
 
 base = app.config['BASE_PATH']
+print base
 app.register_blueprint(api, url_prefix='%s/api' % (base,))
 app.register_blueprint(web, url_prefix='%s/' % (base,))
 app.register_blueprint(login, url_prefix='%s/user' % (base,))
